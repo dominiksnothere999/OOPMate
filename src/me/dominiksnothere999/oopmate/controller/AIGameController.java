8f09cbbd297dda5b441ab128ef2337b340e986fe
dominiksnothere999@gmail.com <dominiksnothere999@gmail.com>
2025-03-23 18:33:20 +0100

    Added a difficulty enum to use for the main menu game difficulty selection.

--- src/me/dominiksnothere999/oopmate/controller/AIGameController.java
+++ src/me/dominiksnothere999/oopmate/controller/AIGameController.java
@@ -1,5 +1,17 @@
 package me.dominiksnothere999.oopmate.controller;
 
 public class AIGameController {
+    private final Difficulty difficulty;
+
+    public AIGameController(Difficulty difficulty) {
+        super();
+        this.difficulty = difficulty;
+    }
     
+    public enum Difficulty {
+        EASY,
+        MEDIUM,
+        HARD
+    }
+
 }
