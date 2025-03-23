bc51cbebe276deb1725aafa287f013190681cfbf
dominiksnothere999@gmail.com <dominiksnothere999@gmail.com>
2025-03-23 16:50:45 +0100

    Finished the Square class.

--- src/me/dominiksnothere999/oopmate/board/Square.java
+++ src/me/dominiksnothere999/oopmate/board/Square.java
@@ -1,5 +1,23 @@
 package me.dominiksnothere999.oopmate.board;
 
+import me.dominiksnothere999.oopmate.pieces.Piece;
+
+// Used for the board of the game to represent a square.
 public class Square {
-    
+    private Piece piece;
+
+    // Constructor for the Square class.
+    public Square() {
+        this.piece = null;
+    }
+
+    // Returns the piece on the square.
+    public Piece getPiece() {
+        return piece;
+    }
+
+    // Sets the piece on the square.
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
 }
