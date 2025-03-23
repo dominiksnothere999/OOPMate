7ae94f70b3c8a44711a472a257a6c9c40bb2ec0c
dominiksnothere999@gmail.com <dominiksnothere999@gmail.com>
2025-03-23 18:40:26 +0100

    Updated the utilities.

--- src/me/dominiksnothere999/oopmate/utils/Util.java
+++ src/me/dominiksnothere999/oopmate/utils/Util.java
@@ -2,13 +2,18 @@ package me.dominiksnothere999.oopmate.utils;
 
 import java.awt.*;
 
+// Utility class for the game.
 public class Util {
+    // Define the colors.
     public static final Color BLACK = new Color(20, 20, 20);
     public static final Color WHITE = new Color(255, 255, 255);
     public static final Color DARK = new Color(50, 70, 120);
     public static final Color LIGHT = new Color(220, 220, 220);
+
+    // Define the square size.
     public static final int SQUARE_SIZE = 60;
 
+    // Define the font.
     public static Font setFont(String type) {
         if (type.equals("STANDARD")) {
             return new Font("SF Pro Display", Font.BOLD, 16);
@@ -21,10 +26,12 @@ public class Util {
         }
     }
 
+    // Define the dimension.
     public static Dimension setDimension(int width, int height) {
         return new Dimension(width, height);
     }
 
+    // Define the grid layout.
     public static GridLayout setGridLayout(int rows, int cols, int hgap, int vgap) {
         return new GridLayout(rows, cols, hgap, vgap);
     }
